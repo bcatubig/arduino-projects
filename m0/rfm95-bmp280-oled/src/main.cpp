b7124400a3abbf9ab7d0e8b8d2f54001fb4a96f7
Brandon Catubig <brandon@notbsd.com>
2019-10-06 01:05:23 -0500

    updates

--- m0/rfm95-bmp280-oled/src/main.cpp
+++ m0/rfm95-bmp280-oled/src/main.cpp
@@ -1,89 +1,38 @@
-#include <Arduino.h>
-
-#include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_BMP280.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Arduino.h>
 #include <RH_RF95.h>
+#include <SPI.h>
+#include <Wire.h>
 #include <avr/dtostrf.h>
 
-// for feather m0
-#define RFM95_CS 8
-#define RFM95_RST 4
-#define RFM95_INT 3
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
 
 // Setup 1306 Display
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 Adafruit_BMP280 bmp;
 
-// Setyp LoRa radio
+// LoRa Settings
+// m0 pins
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
 
-// Set to 915 - US Region
+// Set to US frequency
 #define RF95_FREQ 915.0
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-void setup()
-{
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
-
-  Serial.begin(115200);
-  while (!Serial)
-  {
-    delay(1);
-  }
-
-  delay(100);
-
-  Serial.println("Feather LoRa TX Test!");
-
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-
-  while (!rf95.init())
-  {
-    Serial.println("LoRa radio init failed");
-    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
-    while (1)
-      ;
-  }
-
-  Serial.println("LoRa radio init OK!");
-
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ))
-  {
-    Serial.println("setFrequency failed");
-    while (1)
-      ;
-  }
-
-  Serial.print("Set Freq to: ");
-  Serial.println(RF95_FREQ);
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
-
-  // BMP280 default address is 0x76
-  bmp.begin(0x76);
-
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-  //
-
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+bool init_display() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    return false;
+  }  // Address 0x3C for 128x32
 
   display.display();
   delay(1000);
@@ -96,65 +45,62 @@ void setup()
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.display(); // actually display all of the above
+  display.display();  // actually display all of the above
+
+  return true;
 }
 
-int16_t packetnum = 0;
+bool init_bmp() {
+  // BMP280 default address is 0x76
+  if (!bmp.begin(0x76)) {
+    return false;
+  }
 
-void loop()
-{
-  display.clearDisplay();
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
+  return true;
+}
+
+bool init_lora() {
+  if (!rf95.init()) {
+    Serial.println("Failed to initialize LoRa radio");
+    return false;
+  }
+  return true;
+}
+
+void read_weather_sensor() {
   float current_temp, temp_f, current_pressure, current_altitude;
-  current_temp = bmp.readTemperature();         // Returns temp in Celsius
-  temp_f = current_temp * 1.8 + 32;             // Murica'
-  current_pressure = bmp.readPressure();        // I bought the wrong sensor so ya'll get pressure
-  current_altitude = bmp.readAltitude(1013.25); /* Adjusted to local forecast! */
+  current_temp = bmp.readTemperature();  // Returns temp in Celsius
+  temp_f = current_temp * 1.8 + 32;      // Murica'
+  current_pressure =
+      bmp.readPressure();  // I bought the wrong sensor so ya'll get pressure
+  current_altitude =
+      bmp.readAltitude(1013.25); /* Adjusted to local forecast! */
+}
 
-  display.print(F("Temp: "));
-  display.print(temp_f);
-  display.print(" ");
-  display.print((char)247);
-  display.println("F");
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
 
-  display.print(F("Pressure: "));
-  display.print(current_pressure);
-  display.println(" Pa");
-
-  display.print(F("Alt: "));
-  display.print(current_altitude);
-  display.println(" m");
-
-  /*
-  LoRA THings
-  */
-
-  char loraPacket[20];
-
-  // Convert float to char *
-  dtostrf(temp_f, 4, 3, loraPacket);
-
-  // SEND IT
-  rf95.send((uint8_t *)loraPacket, sizeof(loraPacket));
-
-  // Wait
-  if (!rf95.waitPacketSent())
-  {
-    Serial.println("failed to send lora packet");
+  if (!init_bmp()) {
+    Serial.println("Failed to initalize BMP Sensor");
     while (1)
       ;
   }
-  Serial.println("YEET. Packet sent.");
-  packetnum++;
 
-  display.print("LoRa: Sent ");
-  display.print(packetnum);
-  display.println(" packets");
-
-  display.setCursor(0, 0);
-  display.display();
-
-  // Sleep 10 seconds
-  delay(10000);
-  yield();
+  if (!init_display()) {
+    Serial.println("Failed to initalize OLED");
+    while (1)
+      ;
+  }
 }
+
+void loop() {}
